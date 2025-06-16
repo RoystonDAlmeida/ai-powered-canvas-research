e60f804e1f491fb5cc94aa6edeaf250974be2952
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-06-16 19:26:34 +0530

    Changed the name of the app

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -4,12 +4,12 @@ interface AppHeaderProps {
   description?: string;
 }
 
-const AppHeader: React.FC<AppHeaderProps> = ({ description }) => {
+const AppHeader: React.FC<AppHeaderProps> = () => {
   return (
     <header className="bg-blue-700 text-white py-3 font-medium text-xl md:text-2xl fixed top-0 left-0 w-full z-40">
       <div className="max-w-screen-xl mx-auto flex items-center pr-96">
         <h1 className="flex-grow text-center font-serif">
-          AI-Powered Research Canvas {description && `- ${description}`}
+          Insight Canvas AI
         </h1>
       </div>
     </header>
